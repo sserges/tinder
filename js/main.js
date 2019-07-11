cb31b6397e7ff969b00f03611bbc6217e3c43207
sserges <agounkeserge@gmail.com>
2019-07-11 15:41:34 +0000

    Animations added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-  alert("Hello world !");
+  // Hide Overlay on Video Play
+  $("#video_background").on("play", function() {
+    $(".overlay").css("opacity", 0);
+  });
+
+  // Display other languages on click
+  $("nav li.active a").on("click", function(e) {
+    e.preventDefault();
+    // $("nav li:not(.active)").slideToggle(400);
+    $(this)
+      .closest("li")
+      .siblings("li")
+      .slideToggle(400);
+  });
+  // Magnific Popup
+  // $(".popup").magnificPopup({
+  //   type: "iframe",
+  //   removalDelay: 5000,
+  //   showCloseBtn: false
+  // });
+
+  // Arrow Down Button
+  $(".arrow_down").on("click", function(e) {
+    e.preventDefault();
+    $(this).removeClass("bounce");
+
+    $("html, body").animate(
+      {
+        scrollTop: $(document).height()
+      },
+      "slow"
+    );
+  });
 });
